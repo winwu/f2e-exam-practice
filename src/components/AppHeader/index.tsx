b9ceb7da3300a3a9b297c9bf3fa162d3d8204c06
winwu <look_me08@hotmail.com>
2020-12-27 22:37:58 +0800

    add menu

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Link,
     useLocation
 } from "react-router-dom";
 
-import { chevronLeftWhite } from '../Icons';
+import { chevronLeftWhite, threeDots, bookmarkFill, trash } from '../Icons/index';
 import './AppHeader.scss';
 
 const AppHeader = () => {
+    const [showMenu, toggleMenu] = useState<Boolean>(false);
     const location = useLocation();
+
+    const toToggleMenu = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        toggleMenu(!showMenu);
+    };
+
     return (
         <header className="app-header sticky-top">
             <div className="container container-700">
@@ -21,10 +28,20 @@ const AppHeader = () => {
                     <div className="col-8 text-center">
                         <div className="header-logo">⾦融市場常識與職業道德</div>
                     </div>
-                    <div className="col-2 d-flex justify-content-end align-items-center">
-                        {/* <a className="btn btn-sm btn-outline-secondary" href="#">List</a> */}
+                    <div className="col-auto d-flex justify-content-end align-items-center">
+                        <button className={`btn btn-sm btn-menu ${showMenu ? 'active' : ''}`} onClick={toToggleMenu}>{threeDots}</button>
                     </div>
                 </div>
+            </div>
+            <div className={`menu ${showMenu ? 'active' : ''}`}>
+                <ul>
+                    <li>
+                        <Link to="/"><i className="menu-icon">{bookmarkFill}</i>書籤列表</Link>
+                    </li>
+                    <li>
+                        <button><i className="menu-icon">{trash}</i>清除作答記錄</button>
+                    </li>
+                </ul>
             </div>
         </header>
     );
--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,3 +1,4 @@
+// via https://icons.getbootstrap.com/
 // import bookmark from 'bootstrap-icons/icons/bookmark.svg';
 // import bookmarkFill from 'bootstrap-icons/icons/bookmark-fill.svg';
 
@@ -18,3 +19,15 @@ export const chevronLeftWhite = (
         <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
     </svg>
 );
+
+export const threeDots = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots" viewBox="0 0 16 16">
+        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+    </svg>
+)
+
+export const trash = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+    </svg>
+)
